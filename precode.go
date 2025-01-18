20e0cbec3ff90773ee740fd16d450b7bf5335017
vslvgit <147726876+vslvgit@users.noreply.github.com>
2025-01-18 13:27:52 +0300

    Revert "first commit"

--- precode.go
+++ precode.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -29,7 +28,7 @@ var tasks = map[string]Task{
 	},
 	"2": {
 		ID:          "2",
-		Description: "Протестировать финальное задание с помощью Postman",
+		Description: "Протестировать финальное задание с помощью Postmen",
 		Note:        "Лучше это делать в процессе разработки, каждый раз, когда запускаешь сервер и проверяешь хендлер",
 		Applications: []string{
 			"VS Code",
@@ -40,99 +39,14 @@ var tasks = map[string]Task{
 	},
 }
 
-func getTasks(w http.ResponseWriter, r *http.Request) {
-
-	resp, err := json.Marshal(tasks)
-
-	if err != nil {
-
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(resp)
-
-}
-
-func postTasks(w http.ResponseWriter, r *http.Request) {
-	var task Task
-	decoder := json.NewDecoder(r.Body)
-	defer r.Body.Close()
-	if err := decoder.Decode(&task); err != nil {
-
-		http.Error(w, "Некорректный JSON: "+err.Error(), http.StatusBadRequest)
-		return
-
-	}
-
-	tasks[task.ID] = task
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-}
-
-func getPerIdTasks(w http.ResponseWriter, r *http.Request) {
-
-	id := chi.URLParam(r, "id")
-
-	task, ok := tasks[id]
-
-	if !ok {
-
-		http.Error(w, "Задание не найдено", http.StatusNoContent)
-
-		return
-
-	}
-
-	resp, err := json.Marshal(task)
-
-	if err != nil {
-
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(resp)
-
-}
-
-func deletePerIdTasks(w http.ResponseWriter, r *http.Request) {
-
-	id := chi.URLParam(r, "id")
-
-	if r.Method != http.MethodDelete {
-
-		http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
-		return
-
-	}
-
-	if _, exists := tasks[id]; !exists {
-
-		http.Error(w, "Задание не найдено", http.StatusNotFound)
-		return
-
-	}
-
-	delete(tasks, id)
-
-	w.WriteHeader(http.StatusNoContent)
-}
+// Ниже напишите обработчики для каждого эндпоинта
+// ...
 
 func main() {
-
 	r := chi.NewRouter()
 
-	r.Get("/tasks", getTasks)
-	r.Post("/tasks", postTasks)
-	r.Get("/tasks/", getPerIdTasks)
-	r.Delete("/tasks/", deletePerIdTasks)
+	// здесь регистрируйте ваши обработчики
+	// ...
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
